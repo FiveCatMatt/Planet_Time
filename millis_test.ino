d16f7a78755de3fdb413266f4f8052948e30a2ba
FiveCatMatt <matthewclawrence@yahoo.com>
2016-12-02 11:10:55 -0700

    Add files via upload

---
+++ millis_test.ino
@@ -0,0 +1,20 @@
+long timer;
+long trydelay;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  trydelay++;
+  
+  timer = millis();
+  timer /= 1000;
+  Serial.print("Millis: ");
+  Serial.println(timer);
+  Serial.print("Delay: ");
+  Serial.println(trydelay);
+  delay(2000);                  // Halt all operations for 2 secs
+}
